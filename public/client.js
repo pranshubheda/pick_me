7e3e6c833d4076ebadd32e93eb515ce1e2a456dc
pranshubheda <bhedapranshu@gmail.com>
2020-08-06 00:33:26 -0400

    log fetched member data

--- public/client.js
+++ public/client.js
@@ -1,3 +1,5 @@
+let members = [];
+
 function run_pick_me() {
     fetch('/run_pick_me')
         .then(response => {
@@ -5,9 +7,36 @@ function run_pick_me() {
             throw new Error('Request failed.');
         })
         .then(data => {
-            document.getElementById('test').innerHTML = data.name;
+            document.getElementById('test').innerHTML = JSON.stringify(data);
         })
         .catch(error => {
             console.error('Unsuccessful response');
         })
 }
+
+function get_members() {
+    fetch('/get_members')
+    .then(response => {
+        if(response.ok) return response.json();
+        throw new Error('Request failed.');
+    })
+    .then(data => {
+        members = data;
+    })
+    .catch(error => {
+        console.error('Unsuccessful response');
+    })
+}
+
+function init() {
+    //make api call to fetch members, populate members variable
+    get_members();
+    //populate table
+    for (let i = 0; i < members.length; i++) {
+        const member = members[i];
+        let insert_row_string = `<tr><th scope=\'row\'>${member.id}</th><td>${member.name}</td><td>${member.probability}</td><td>${member.karma}</td></tr>`;
+        console.log(insert_row_string);
+    }
+}
+
+init();
--- server.js
+++ server.js
@@ -14,15 +14,54 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+let data = {
+    members: [
+        {
+            id: 1,
+            name: 'Pranshu',
+            probability: 1,
+            karma: 100
+        },
+        {
+            id: 2,
+            name: 'Prasad',
+            probability: 1,
+            karma: 100
+        },
+        {
+            id: 3,
+            name: 'Madhu',
+            probability: 1,
+            karma: 100
+        },
+        {
+            id: 4,
+            name: 'Sam',
+            probability: 1,
+            karma: 100
+        }
+    ]
+};
+
+/*
+-- Validation -- 
+Check if probability initialization required.
+-- Validation -- 
+Read in the member list.
+Pick a member based on probabiliy.
+Adjust the probability of winner.
+Write the results to file.
+*/
 app.get('/run_pick_me', (req, res) => {
-    console.log("TEST")
-    data = {
-        name: 'Pranshu',
-        probability: 1
-    };
-    res.send(data);
-    // res.send({ hello: 'world' });
+    n = data.members.length
+    picked_index = Math.floor(Math.random() * n);
+    res.send(data.members[picked_index]);
 });
+
+app.get('/get_members', (req, res) => {
+    res.send(data.members);
+});
+
 
 /*
 Use this function to add a new member to be considered 
@@ -56,17 +95,4 @@ of the team members. A fresh start.
 */
 function initialize_probabilities() {
     //pass
-}
-
-/*
--- Validation -- 
-Check if probability initialization required.
--- Validation -- 
-Read in the member list.
-Pick a member based on probabiliy.
-Adjust the probability of winner.
-Write the results to file.
-*/
-function run_pick_me() {
-    return 'Running pick me'
 }
